08bf8b86681bb01f07730bd18e948119a18a5432
Jem Cope <thejemmeh@gmail.com>
2022-05-21 01:13:40 -0700

    Just a Truck on a Road. Truck basic movement

---
+++ TruckKun/Assets/Scripts/TestCharacterController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacterController : MonoBehaviour
+{
+
+    [SerializeField] public float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed / 2;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed;
+        transform.Translate(new Vector3(hMovement, 0, vMovement) * Time.deltaTime);
+    }
+}
